7cf6e23eb2a9182a651ad7cd13548eccfebe8248
Maxim <yourstenex@gmail.com>
2022-05-28 20:32:24 +0300

    main, changed api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5000"
+      : "https://backend-web-news.herokuapp.com/",
 });
 
 const getToken = (): string | null => {
